d3ab8b119ffa388a237886f6dbcdb85cff6b41c4
Cici <cici@email.com>
2022-01-11 15:18:17 +0100

    python file and result for first assignment

---
+++ assignment 1 code.py
@@ -0,0 +1,32 @@
+# setting up appropriate data structure
+rainfall = {}
+with open("python assignment\stations.csv") as file:
+    columns = file.readline()
+    for line in file:
+        station = line.strip().split(",")
+        rainfall[station[0]] = {
+            "station": station[2],
+            "state": station[1],
+            "totalMonthlyPrecipitation": [],
+            "relativeMonthlyPrecipitation": [],
+            "totalYearlyPrecipitation": 0,
+            "relativeYearlyPrecipitation": 0
+        }
+
+# importing data from datafile
+import json
+with open("python assignment\precipitation.json") as file:
+    data = json.load(file)
+
+# defining relevant variables and sorting data per month in Seattle
+for i in range(1,13):
+    rainfall["Seattle"]["totalMonthlyPrecipitation"].append(0)  # adding 12 values, one per month
+for measurement in data:
+    if measurement["station"] == rainfall["Seattle"]["station"]:  # islolating data to Seattle
+            date = measurement["date"].split("-")  # splitting date key into list
+            month = int(date[1])  # calculating month per measurement
+            rainfall["Seattle"]["totalMonthlyPrecipitation"][int(month - 1)] += int(measurement["value"]) # adding value into monthly measurement
+
+# saving results for assignment 1
+with open("python assignment/result1.json", "w") as file:
+    json.dump(rainfall, file)
